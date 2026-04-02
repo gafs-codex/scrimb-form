cb6d76b129b188fba3e0ff575464be690e660f52
gafs-codex <abdulmuizgafarcodes@gmail.com>
2026-04-02 18:29:56 +0100

    more about forms

--- src/App.jsx
+++ src/App.jsx
@@ -35,8 +35,11 @@ function App() {
     // and also resets theform automatically for us and other things
     const email = formData.get("email")
     const password = formData.get("password")
+    const jobStatus = formData.get("employmentStatus");
     console.log(email);
     console.log(password);
+    console.log(jobStatus);
+
   }
   return (
     <section>
@@ -44,12 +47,35 @@ function App() {
       <form action={signUp} /*onSubmit={handleSubmit}*/ >
         <label htmlFor="email">Email:</label>
         {/*name attribute will be used to access data that is inputted into the input by user*/}
-        <input id="email" type="email" name="email" placeholder="Joe@schmoe.com" />
+        <input id="email" type="email" name="email" defaultValue="joe@schmoe.com" placeholder="Joe@schmoe.com" />
         <br />
         <br />
 
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name='password' />
+        <input type="password" defaultValue="password123" id="password" name='password' />
+
+        <label htmlFor="description">Description:</label>
+        <textarea name="description" id="description" defaultValue="This is a description"></textarea>
+
+        <fieldset>
+          <legend>Employment Status</legend>
+
+          <label htmlFor="">
+            <input type="radio" name='employmentStatus' value="Uemployed" defaultChecked={true} />
+            Unemployed
+          </label>
+
+          <label htmlFor="">
+            <input type="radio" name='employmentStatus' value="Part-Time"/>
+            Part-Time
+          </label>
+
+          <label htmlFor="">
+            <input type="radio" name='employmentStatus' value="Full-Time"/>
+            Full-Time
+          </label>
+        </fieldset>
+
 
         <button>Submit</button>
       </form>
