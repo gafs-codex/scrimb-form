4c35c38b928065c761496f9bbe47dc5a337293bd
gafs-codex <abdulmuizgafarcodes@gmail.com>
2026-04-02 17:48:28 +0100

    forms using react

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+// import './App.css'
+import './form.css'
+
+
+// Form basics in react
+function App() {
+
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   console.log("submitted");
+  //   const formEl = event.currentTarget
+  //   const formData = new FormData(formEl)
+  //   {/* using the name attributes from the input */ }
+  //   const email = formData.get("email");
+  //   const password = formData.get("password")
+  //   console.log(email)
+  //   console.log(password);
+  //   formEl.reset();
+  // }
+
+
+
+  console.log("hey");
+
+  // react-19 makes things easier
+
+  // so the sign up function doesn't receive an event
+  // it automatically receives the formdata
+  function signUp(formData) {
+    // behind the scenes the action will prevent default
+    // and also resets theform automatically for us and other things
+    const email = formData.get("email")
+    const password = formData.get("password")
+    console.log(email);
+    console.log(password);
+  }
+  return (
+    <section>
+      <h1>Sign up form</h1>
+      <form action={signUp} /*onSubmit={handleSubmit}*/ >
+        <label htmlFor="email">Email:</label>
+        {/*name attribute will be used to access data that is inputted into the input by user*/}
+        <input id="email" type="email" name="email" placeholder="Joe@schmoe.com" />
+        <br />
+        <br />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name='password' />
+
+        <button>Submit</button>
+      </form>
+    </section>
+  )
+}
+
+export default App
